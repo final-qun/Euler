0557a5d22956c2c550ad5a68a6b3c1d4c22ce048
shamaker <990951910@qq.com>
2016-05-09 10:40:43 +0800

    modify

--- src/com/quicksand/problem/Lexicographic.java
+++ src/com/quicksand/problem/Lexicographic.java
@@ -9,23 +9,33 @@ import java.util.List;
  *
  */
 public class Lexicographic {
-	public static int[] ranke = new int[9];
+	public static List<Integer> ranke = new ArrayList<Integer>();
 	public static List<Integer> rank = new ArrayList<Integer>();
 	public static List<Integer> def = new ArrayList<Integer>();
-	public static final int NUM_OFFSET = 10;
-	public static void main(String[] args) {
-		for(int i = 0;i < ranke.length;i++){
-			ranke[i] = getNumOfRanke(9-i);
-		}
+	public static void main(String[] args) {		
 		//从第0个开始算起，所以应该减1
 		int ranked = 1000000-1;
-		System.out.println(ranked);
-		for(int i = 0;i < 10;i++){
+		try {
+			getRanked(ranked, 9);
+		} catch (Exception e) {
+			System.out.println("没有这么多的排列");
+		}
+	}
+	/**
+	 * 获取第ranked位，0~num的全排列的数
+	 * @param ranked 
+	 * @param num
+	 */
+	public static void getRanked(int ranked,int num) throws IndexOutOfBoundsException{
+		saveRankeOfNum(num);
+		//添加默认的排列组合
+		for(int i = 0;i <= num;i++){
 			def.add(i);
 		}
-		for(int i = 0;i<ranke.length;i++){
-			int shang = ranked / ranke[i];
-			int yu = ranked % ranke[i];
+		//生成排列的每一位
+		for(int i = 0;i<ranke.size();i++){
+			int shang = ranked / ranke.get(i);
+			int yu = ranked % ranke.get(i);
 			if(rank!=null){
 				rank.add(def.get(shang));
 				def.remove(shang);
@@ -40,7 +50,11 @@ public class Lexicographic {
 			System.out.print(rank.get(i));
 		}
 	}
-	
+	public static void saveRankeOfNum(int num){
+		for(int i = 0;i<num;i++){
+			ranke.add(getNumOfRanke(num-i));
+		}
+	}
 	public static int getNumOfRanke(int num){
 		if(num == 1){
 			return 1;
